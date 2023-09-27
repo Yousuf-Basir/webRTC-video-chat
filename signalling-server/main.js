905969f620ff46a12c5c7acb840b8451d389ac61
yousuf <gourabxz@gmail.com>
2023-09-27 18:30:57 +0600

    fix api port

--- signalling-server/main.js
+++ signalling-server/main.js
@@ -59,7 +59,7 @@ app.get("/", (req, res) => {
     res.send("Server running");
 });
 
-const PORT = process.send.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
