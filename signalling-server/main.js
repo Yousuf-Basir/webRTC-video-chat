ed2a13570bb92cdadf984cf4297d00272d8a21cc
yousuf <gourabxz@gmail.com>
2024-03-27 20:48:56 +0600

    Update PORT variable in main.js

--- signalling-server/main.js
+++ signalling-server/main.js
@@ -83,7 +83,7 @@ app.get("/", (req, res) => {
     res.send("Server running");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3210;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
