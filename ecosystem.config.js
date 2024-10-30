f79332793e082146dcbf0e436b99e8b85cec0a80
yousuf <gourabxz@gmail.com>
2024-10-30 16:49:02 +0600

    update run script

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps: [
+      {
+        name: 'webrtc-video-chat-frontend',
+        script: 'npm',
+        args: 'run frontend',
+        cwd: './',
+        watch: false,
+        env: {
+          NODE_ENV: 'production'
+        }
+      },
+      {
+        name: 'webrtc-video-chat-backend',
+        script: 'npm',
+        args: 'run backend',
+        cwd: './',
+        watch: false,
+        env: {
+          NODE_ENV: 'production'
+        }
+      }
+    ]
+  }
--- signalling-server/main.js
+++ signalling-server/main.js
@@ -83,7 +83,7 @@ app.get("/", (req, res) => {
     res.send("Server running");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 57810;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
